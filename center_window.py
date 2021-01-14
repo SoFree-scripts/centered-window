ac59513418ac65cce647ae14c11e72d4004c70a9
SoFree-scripts <76851715+SoFree-scripts@users.noreply.github.com>
2021-01-14 10:21:15 +0000

    Add files via upload

--- center_window.py
+++ center_window.py
@@ -1,11 +1,22 @@
 from win32api import GetMonitorInfo, MonitorFromPoint
 import os
+import ctypes
+import tkinter
+
+import tkinter
+root = tkinter.Tk()
+dpi = root.winfo_fpixels('1i')
+root.destroy()
+
 monitor_info = GetMonitorInfo(MonitorFromPoint((0,0)))
 work_area = monitor_info.get("Work")
 monitor_area = monitor_info.get("Monitor")
 
-taskbarheight = monitor_area[3]-work_area[3]
 
+
+taskbarheight = monitor_area[3]-work_area[3]
+dpi = dpi / 100
+taskbarheight *= dpi
 path = "."
 cwCfg = os.path.join(path,"sofplus/data/cw_work_area.cfg")
 if not os.path.exists(cwCfg):
