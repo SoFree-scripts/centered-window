fd75a2f81f8e942141e8cb1a9116cbd748c056dc
SoFree-scripts <76851715+SoFree-scripts@users.noreply.github.com>
2021-01-14 10:47:15 +0000

    Add files via upload
    removed incorrect dpi scaling method. the search continues for win7 (win10 solution exists)

--- center_window.py
+++ center_window.py
@@ -3,11 +3,6 @@ import os
 import ctypes
 import tkinter
 
-import tkinter
-root = tkinter.Tk()
-dpi = root.winfo_fpixels('1i')
-root.destroy()
-
 monitor_info = GetMonitorInfo(MonitorFromPoint((0,0)))
 work_area = monitor_info.get("Work")
 monitor_area = monitor_info.get("Monitor")
@@ -15,8 +10,7 @@ monitor_area = monitor_info.get("Monitor")
 
 
 taskbarheight = monitor_area[3]-work_area[3]
-dpi = dpi / 100
-taskbarheight *= dpi
+
 path = "."
 cwCfg = os.path.join(path,"sofplus/data/cw_work_area.cfg")
 if not os.path.exists(cwCfg):
